e0293f7cc5901ea1829ecc4dcc4cdd7919fb7c39
Vitalya.Efimov <Vitalay.efimov@gmail.com>
2024-11-13 17:42:26 +0500

    добавил кнопку выводит сообщение

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log(1)
+const button = document.querySelector('button');
+const autput = document.querySelector('.autput');
+button.addEventListener('click', function() {
+    autput.innerHTML = 'Мама я тебя люблю';
+
+})
